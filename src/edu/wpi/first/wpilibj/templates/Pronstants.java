2b215fb2ab78bda71a4f378395f7d6b4e51326ef
Max Kelton <maxwellk@mtwk.com>
2015-02-06 16:55:07 -0800

    fixed id's

--- src/edu/wpi/first/wpilibj/templates/Pronstants.java
+++ src/edu/wpi/first/wpilibj/templates/Pronstants.java
@@ -5,8 +5,8 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 
 public interface Pronstants {
     //compressor values
-    public final static int COMPRESSOR_1 = 1;
-    public final static int COMPRESSOR_2 = 13;
-    public final static int COMPRESSOR_3 = 1;
-    public final static int COMPRESSOR_4 = 8;
+    public final static int COMPRESSOR_ID1 = 1;
+    public final static int COMPRESSOR_ID2 = 13;
+    public final static int COMPRESSOR_ID3 = 1;
+    public final static int COMPRESSOR_ID4 = 8;
 }
